df8c602c2df37657a655c6b183f132055a5df091
VectorShadow <ncbrayshaw@hotmail.com>
2021-07-25 09:42:51 -0500

    Updates to support Login on client side.

--- src/vsdl/wl/elements/DataMessageHeaders.java
+++ src/vsdl/wl/elements/DataMessageHeaders.java
@@ -1,6 +1,8 @@
 package vsdl.wl.elements;
 
 public class DataMessageHeaders {
-    public static final String PUBLIC_KEY = "PUBKEY";
-    public static final String SESSION_KEY = "SESKEY";
+    public static final String PUBLIC_KEY = "PUBLIC_KEY";
+    public static final String SESSION_KEY = "SESSION_KEY";
+    public static final String LOGIN_ACCOUNT = "LOGIN_ACCOUNT";
+    public static final String CREATE_ACCOUNT = "CREATE_ACCOUNT";
 }
