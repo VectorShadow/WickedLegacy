ed768a2a57673595f029efff3d212118d04fd1f4
VectorShadow <ncbrayshaw@hotmail.com>
2021-07-27 17:09:40 -0500

    Salting is now done server side, not client side.
    Support for future file management.

--- src/vsdl/wl/data/DataProvider.java
+++
@@ -1,9 +0,0 @@
-package vsdl.wl.data;
-
-public interface DataProvider {
-    void queryAccount(String username, String hashedPassword);
-    void createAccount(String username, String hashedPassword);
-    String getSalt(String username);
-    String getHash(String username);
-    //todo
-}
