f387a4ae24c51abf0afed80e51ec581096f290d2
VectorShadow <ncbrayshaw@hotmail.com>
2021-08-08 10:18:47 -0500

    Added password validation.

--- src/vsdl/wl/elements/DataMessageErrors.java
+++ src/vsdl/wl/elements/DataMessageErrors.java
@@ -2,4 +2,5 @@ package vsdl.wl.elements;
 
 public class DataMessageErrors {
     public static final String LOGON_USER_DID_NOT_EXIST = "LOGON_USER_DID_NOT_EXIST";
+    public static final String LOGON_INCORRECT_PASSWORD = "LOGON_INCORRECT_PASSWORD";
 }
