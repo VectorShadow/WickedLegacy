090a312fc8867fdd75e32ea0f3ffc8ea1ea135ad
VectorShadow <ncbrayshaw@hotmail.com>
2021-08-08 09:19:19 -0500

    Increased cross module logon support.

---
+++ src/vsdl/wl/elements/DataMessageErrors.java
@@ -0,0 +1,5 @@
+package vsdl.wl.elements;
+
+public class DataMessageErrors {
+    public static final String LOGON_USER_DID_NOT_EXIST = "LOGON_USER_DID_NOT_EXIST";
+}
--- src/vsdl/wl/elements/DataMessageHeaders.java
+++ src/vsdl/wl/elements/DataMessageHeaders.java
@@ -3,6 +3,7 @@ package vsdl.wl.elements;
 public class DataMessageHeaders {
     public static final String PUBLIC_KEY = "PUBLIC_KEY";
     public static final String SESSION_KEY = "SESSION_KEY";
-    public static final String LOGIN_ACCOUNT = "LOGIN_ACCOUNT";
-    public static final String CREATE_ACCOUNT = "CREATE_ACCOUNT";
+    public static final String LOGON_USER = "LOGON_USER";
+    public static final String CREATE_USER = "CREATE_USER";
+    public static final String LOGON_ERR = "LOGON_ERR";
 }
