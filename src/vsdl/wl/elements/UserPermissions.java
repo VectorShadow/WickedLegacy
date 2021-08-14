278b4227e6133f5109750beca7592ebdfe55d04e
VectorShadow <ncbrayshaw@hotmail.com>
2021-08-14 15:51:32 -0500

    Removed Updater class and replaced with a .cql file to contain update queries, and an update to the startup script to run them automatically on startup.

---
+++ src/vsdl/wl/elements/UserPermissions.java
@@ -0,0 +1,8 @@
+package vsdl.wl.elements;
+
+public class UserPermissions {
+    public static final int REVOKED = -1;
+    public static final int DEFAULT = 0;
+    public static final int ELEVATED = 1;
+    public static final int ROOT = 10;
+}
